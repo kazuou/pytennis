130b9748839f7e552757adf88177e720b5036a2a
kazuo <kazuou0730@yahoo.co.jp>
2020-05-15 17:06:08 +0900

    add makepng.py

--- makepng.py
+++ makepng.py
@@ -19,7 +19,7 @@ pygame.key.set_repeat(5, 5)
 #SURFACE = pygame.display.set_mode((1200, 600))
 SURFACE = pygame.display.set_mode((400, 600))
 FPSCLOCK = pygame.time.Clock()
-pygame.display.set_caption("Tobishokunin's Commuting")
+pygame.display.set_caption("makepng")
 sysfont = pygame.font.SysFont(None, 24)
 
 baseline2 = 2377
@@ -35,20 +35,12 @@ fieldy2=2377+1000
 netline=1188.5
 field_data = (fieldxl,fieldy1,fieldxr,fieldy2)
 
-
-
 def location_in_view(x1, y1, z1, size_x, size_z, adj_x, adj_y, adj_z):
     """ 3D座標から2D上の座標算出 """
     x1=x1/2
     y1=(y1+1000-adj_y)/100 #10m後ろからの視点
     if(y1 <= 0):
         y1 = 0
-    x2 = int((x1 + adj_x * y1) / (y1/10 + 1)) + 200 - int(size_x * 0.5 / (y1/10 + 1))
-    z2 = int((z1 + adj_z * y1) / (y1/10 + 1)) + 150 - int(size_z * 0.5 / (y1/10 + 1))
-    return (x2, z2)
-
-def location_in_view2(x1, y1, z1, size_x, size_z, adj_x, adj_y, adj_z):
-    """ 3D座標から2D上の座標算出 """
     x2 = int((x1 + adj_x * y1) / (y1/10 + 1)) + 200 - int(size_x * 0.5 / (y1/10 + 1))
     z2 = int((z1 + adj_z * y1) / (y1/10 + 1)) + 150 - int(size_z * 0.5 / (y1/10 + 1))
     return (x2, z2)
@@ -69,7 +61,7 @@ def draw_background(adjust_x, adjust_y, adjust_z, counter):
     pygame.draw.rect(SURFACE, (189,104,86), (0, b, 400, 300))
 
     #テニスコートのサイズ　ネットの位置は1188.5
-#   court_data = (-548.5,2377,543.5,0)
+    #court_data = (-548.5,2377,543.5,0)
     x1,y1,x2,y2 = court_data
 
     pygame.draw.polygon(SURFACE,(0, 128, 0), #テニスコート
@@ -92,6 +84,9 @@ def main():
     a, b, c, d = 0, 0, 0, 0
     round_data = [(50, 0, 0), (50, 0, 0), (0, 80, 0), (0, 70, 0), (50, 80, 0), (50, 70, 0), (10, 0, 0), (15, 0, 0), (0, 0, 100), (0, 0, 50),
                   (0, 30, 0), (0, 30, 0), (30, 0, 50), (30, 0, 50), (50, 50, 50), (50, 50, 50), (30, 40, 0), (15, 40, 40), (15, 30, 30), (0, 0, 0)]
+
+    counter = 0
+    print("Hello World")
 
     #マウスカーソル非表示
     pygame.mouse.set_visible(False)
@@ -134,7 +129,10 @@ def main():
                 a = 1
             elif pressed[K_SPACE] == 0 and a == 1:
                 a = 0
+                pygame.image.save(SURFACE,"image.png")
                 gameover_flag = 0
+                pygame.quit()
+                sys.exit()
 
             #画面再描写(フルスクリーン対応)
             SURFACE.blit(filler, (0,0))
@@ -143,3 +141,7 @@ def main():
             #画面アップデート
             pygame.display.update()
             FPSCLOCK.tick(30)
+
+
+if __name__ == '__main__':
+    main()
